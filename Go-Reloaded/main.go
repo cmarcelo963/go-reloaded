b944e613a66276e653c06a27f834cbdfff231dff
cmarcelo963 <ianmarcelo963@gmail.com>
2021-11-07 05:03:20 +0000

    GoReloaded

--- Go-Reloaded/main.go
+++ Go-Reloaded/main.go
@@ -6,13 +6,22 @@ import (
 
 func main() {
 	file := gr.ReadFile()
+
 	file = gr.ToHex(file)
+
 	file = gr.ToBin(file)
+
 	file = gr.ToUp(file)
+
 	file = gr.ToLow(file)
+
 	file = gr.ToCap(file)
+
 	file = gr.Punc(file)
+
 	file = gr.A(file)
-	
+
+	file = gr.Finalise(file)
+
 	gr.CreateFile(file)
 }
